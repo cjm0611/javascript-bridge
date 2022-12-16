9c75570266fc8f91a762609bf7b5960cd0c7472a
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-12-16 11:46:30 +0900

    feat: 생성한 다리 BridgeGame 필드로 set

--- src/controller/BridgeGameController.js
+++ src/controller/BridgeGameController.js
@@ -3,8 +3,11 @@ const { GUIDE_MESSAGES } = require('../constant/Messages');
 const InputView = require('../view/InputView');
 const BridgeMaker = require('../BridgeMaker');
 const BridgeRandomNumberGenerator = require('../BridgeRandomNumberGenerator');
+const BridgeGame = require('../model/BridgeGame');
 
 class BridgeGameController {
+  #game = new BridgeGame();
+
   start() {
     Console.print(GUIDE_MESSAGES.gameStart);
     this.makeBridge();
@@ -12,7 +15,9 @@ class BridgeGameController {
 
   makeBridge() {
     const onDeliveryBridgeSize = (size) => {
-      BridgeMaker.makeBridge(Number(size), BridgeRandomNumberGenerator.generate);
+      const generateRandomNumber = BridgeRandomNumberGenerator.generate;
+      const bridge = BridgeMaker.makeBridge(Number(size), generateRandomNumber);
+      this.#game.setBridge(bridge);
     };
 
     InputView.readBridgeSize(onDeliveryBridgeSize);
